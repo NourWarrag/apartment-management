4370bc419b4277b8c14cfdbe0de49cace12dcfa8
Hotel Admin <nouralhudawarrag@gmail.com>
2026-05-13 14:20:35 +0400

    fix: RTL-aware main content offset in AppLayout

--- client/src/components/layout/AppLayout.tsx
+++ client/src/components/layout/AppLayout.tsx
@@ -6,7 +6,7 @@ export default function AppLayout() {
   return (
     <div className="min-h-screen bg-background">
       <Sidebar />
-      <div className="ml-[280px] flex flex-col min-h-screen">
+      <div className="ltr:ml-[280px] rtl:mr-[280px] flex flex-col min-h-screen">
         <TopBar />
         <main className="flex-1 p-container-padding">
           <Outlet />
